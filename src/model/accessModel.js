5226d3ec378d54d6253dfab76ad76f603374fd34
Farah Nasreen <farahnasreen2000@gmail.com>
2023-08-24 13:41:12 +0530

    Refactor

--- src/model/accessModel.js
+++ src/model/accessModel.js
@@ -1,11 +1,9 @@
 import mongoose, { Schema } from "mongoose";
-console.log("inside model")
 
-const registerSchema = new Schema({
+const userSchema = new Schema({
     username: {type: String, require: true},
     email: {type: String, require: true},
     password: {type: String, require: true}
 })
-
-const userRegister = mongoose.model("cryptoCoin", registerSchema);
-export default userRegister;
+const UserModel = mongoose.model("cryptoCoin", userSchema);
+export default UserModel;
